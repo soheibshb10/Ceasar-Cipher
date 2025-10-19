6aee08dc4b8875dafed96e9e46791aaac614adc4
soheib benchabana <97317629+soheibshb10@users.noreply.github.com>
2025-10-19 04:48:25 -0600

    Update ceasar_cipher.py

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -1,14 +1,23 @@
 import string
 
+def caesar_cipher(text, key):
+    shifted_text = ''
+    for char in text:
+        if char.isupper():
+            shifted_text += chr(ord('A') + (ord(char) - ord('A') + key) % 26)
+        elif char.islower():
+            shifted_text += chr(ord('a') + (ord(char) - ord('a') + key) % 26)
+        else:
+            shifted_text += char
+    return shifted_text
+
+
 text=input("Enter the text:")
+key =int(input("Enter the shift key (number):"))
+shifted_text=''
+text=str(text)
 
-text=string.ascii_letters
-shifted_text=''.join(
-chr((ord(char)-ord('A')+3)%26 +ord('A')) if char.isupper() else
-chr((ord(char)-ord('a')+3)%26 +ord('a')) if char.islower() else char
-for char in text
-)
-
+shifted_text=caesar_cipher(text,key)
 
 print(f"Original text: {text}")
 print(f"Shifted text: {shifted_text}")
